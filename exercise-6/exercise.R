cce907577e9dc4ade88dc179d79c5a21abf75cc0
Benjamin <Benjamin.z.jin@gmail.com>
2016-10-20 15:24:46 -0700

    completed exercise

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -5,19 +5,34 @@
 library(nycflights13)
 library(dplyr)
 
-
 # Create a dataframe of the average arrival delay for each destination, then use left_join
 # to join on the "airports" dataframe, which has the airport info
-
+avg.arr.delay.by.dest <- flights %>%
+                         group_by(dest) %>%
+                         summarise(avg.arr.delay = mean(arr_delay, na.rm = TRUE)) %>%
+                         mutate(faa = dest) %>%
+                         left_join(airports, by = 'faa') %>%
+                         arrange(-avg.arr.delay)
 
 # Create a dataframe of the average arrival delay for each airline, then use left_join
 # to join on the "airlines" dataframe, which has the airline info
-
+avg.arr.delay.by.airline <- flights %>%
+                            group_by(carrier) %>%
+                            summarise(arr.delay = mean(arr_delay, na.rm = TRUE)) %>%
+                            left_join(airlines, by = 'carrier') %>%
+                            arrange(-arr.delay)
 
 
 ### Bonus ###
 # Calculate the average delay by city AND airline, then merge on the city and airline information
-
+avg.arr.delay.by.city.and.airline <- flights %>%
+                                     group_by(dest, carrier) %>%
+                                     summarise(arr.delay = mean(arr_delay, na.rm = TRUE)) %>%
+                                     left_join(airlines, by = 'carrier') %>%
+                                     mutate(faa = dest) %>%
+                                     left_join(airports, by = 'faa') %>%
+                                     ungroup() %>%
+                                     arrange(-arr.delay)
 
 # If you're running into sorting issues:
 # http://stackoverflow.com/questions/26555297/dplyr-arrange-a-grouped-df-by-group-variable-not-working
